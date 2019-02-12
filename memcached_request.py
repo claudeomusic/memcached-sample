e738354d4854efbeb67cc1b139debc857c4df213
Chris Changchien <chris.changchien@gmail.om>
2019-02-12 00:31:22 -0800

    Refactoring, better naming.

--- memcached_request.py
+++ memcached_request.py
@@ -4,7 +4,7 @@ class MemcachedRequest:
     """
 
     def __init__(self, request, db):
-        self.args = request.recv(1024).strip().decode('utf-8').split(' ')
+        self.update(request)
         self.db = db
         self.cache = {}
 
@@ -16,28 +16,30 @@ class MemcachedRequest:
     def requires_additional_data(self):
         return self.command in ['set']
 
+    def update(self, request):
+        self.args = self._parse_request(request)
+
     def execute(self, followup=False):
         if followup:
             self.cache['data'] = self.args[0]
-            return self.set_cache()
+            return self._set_cache()
 
         options = {
-            'set': self.prep_set_cache,
-            'get': self.get_cache,
-            'delete': self.delete_cache,
+            'set': self._prep_set_cache,
+            'get': self._get_cache,
+            'delete': self._delete_cache,
         }
         try:
             return options[self.command]()
         except KeyError:
-            print('Invalid command.')
-            return False
+            return 'ERROR\n'
 
-    def prep_set_cache(self):
+    def _prep_set_cache(self):
         self.cache['key'] = self.args[1]
         self.cache['flags'] = self.args[2]
         return '\n'
 
-    def set_cache(self):
+    def _set_cache(self):
         if self.db.insert_value(
                 self.cache.get('key', ''), 
                 self.cache.get('flags', ''), 
@@ -47,15 +49,18 @@ class MemcachedRequest:
         else:
             return 'EXISTS\n'
 
-    def get_cache(self):
+    def _get_cache(self):
         key = self.args[1]
         flags, value = self.db.get_value(key)
         if flags and value:
             return 'VALUE %s %s %s\n%s\nEND\n' % (
               key, flags, len(value), value)
 
-    def delete_cache(self):
+    def _delete_cache(self):
         if self.db.delete_key(self.args[1]):
             return 'DELETED\n'
         else:
             return 'NOT_FOUND\n'
+
+    def _parse_request(self, request):
+        return request.recv(1024).strip().decode('utf-8').split(' ')
